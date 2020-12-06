830d0214cdbde7624b586f215815b6a2279c17d5
andrebtw <gwandrewgd@gmail.com>
2020-12-06 10:05:24 +0100

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function myLoop() {         //  create a loop function
         var temps_historic=[]
         var temps = fs.readFileSync('temps.txt', 'utf8');
         console.log(temps);
+        temps_historic.push(temps)
     } catch(e) {
         console.log('Error:', e.stack);
     }
