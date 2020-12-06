8a901db867306ab05172833e3cc900992f1aaac1
andrebtw <gwandrewgd@gmail.com>
2020-12-06 13:08:58 +0100

    update

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var fs = require('fs');
-//var temps_historic = require('./temps_historic.json');
 let temps_var = [];
-
-
 
 
 /*DISPLAYING MIN */
 
-var x = 0;                  //  set your counter to 1
 
 function min_temp() {         //  create a loop function
-  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
     try {
         Array.min = function(temps_var){
         return Math.min.apply( Math, temps_var);
@@ -24,18 +19,12 @@ function min_temp() {         //  create a loop function
     x++;                    //  increment the counter
     if (x < 99999999999999999999) {           //  if the counter < 10, call the loop function
       actual_temp();             //  ..  again which will trigger another
-    }                       //  ..  setTimeout()
-  }, 2500)
+    }
 }
-
 
 /*DISPLAYING MAX TEMP*/
 
-
-var z = 0;                  //  set your counter to 1
-
 function max_temp() {         //  create a loop function
-  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
     try {
         Array.max = function(temps_var){
         return Math.max.apply( Math, temps_var);
@@ -46,14 +35,7 @@ function max_temp() {         //  create a loop function
     } catch(e) {
         console.log('Error:', e.stack);
     }
-    x++;                    //  increment the counter
-    if (x < 99999999999999999999) {           //  if the counter < 10, call the loop function
-      actual_temp();             //  ..  again which will trigger another
-    }                       //  ..  setTimeout()
-  }, 2500)
 }
-
-
 
 
 /*DISPLAYING ACTUAL TEMP*/
@@ -63,19 +45,10 @@ var i = 0;                  //  set your counter to 1
 function actual_temp() {         //  create a loop function
   setTimeout(function() {   //  call a 3s setTimeout when the loop is called
     try {
-
-        //const temps_json = require('./temps_historic.json') //open json
-        //console.log(temps_json) //print json file in console
-        /*
-        var obj = {
-          temps_historic: []
-        };*/
         var temps = fs.readFileSync('temps.txt', 'utf8'); //reads the temps file which is updated every second by a python script
         console.log(temps); //print the txt file
         document.getElementById("actual_temp").innerHTML = temps; //put it in the html
         temps_var.push(temps); //writes the temps variable which is a number in the json variable
-        //var json = JSON.stringify(obj); //converts to json
-        //fs.writeFileSync('temps_historic.json', json); //writes
         console.log(temps_var);
         min_temp();
         max_temp();
@@ -86,6 +59,6 @@ function actual_temp() {         //  create a loop function
     if (i < 99999999999999999999) {           //  if the counter < 10, call the loop function
       actual_temp();             //  ..  again which will trigger another
     }                       //  ..  setTimeout()
-  }, 2500)
+
 }
 actual_temp();                   //  start the loop
