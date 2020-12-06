7bf22092cfda6c5d3a41c28ccc0cae6f2d27572c
andrebtw <gwandrewgd@gmail.com>
2020-12-06 10:58:48 +0100

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function actual_temp() {         //  create a loop function
         var temps = fs.readFileSync('temps.txt', 'utf8');
         console.log(temps);
         document.getElementById("actual_temp").innerHTML = temps;
-        obj.temps_historic.push({temperature_historic: temps});
+        obj.temps_historic.push({temps});
         var json = JSON.stringify(obj);
         fs.writeFileSync('temps_historic.json', json);
     } catch(e) {
